d7032c7caef03360f12b46bd023d443c11d24732
alamin <alamin@gmail.com>
2021-11-26 02:08:31 +0600

    add express, make user model and save data in db

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
 
 mongoose.connect(
   "mongodb+srv://test:test@demo.erwyk.mongodb.net/TestData?retryWrites=true&w=majority",
@@ -8,78 +7,7 @@ mongoose.connect(
   }
 );
 
-/**
- * Make a simple schema for store data
- */
-
-// const User = mongoose.model("User", {
-//   name: {
-//     type: String,
-//   },
-//   age: {
-//     type: Number,
-//   },
+// const task = new Task({
+//   name: "Alamin Sheikh",
+//   email: "alamin@gmail.com",
 // });
-
-// /**
-//  * initial data
-//  */
-
-// const me = new User({
-//   name: "Alamin",
-//   age: 22,
-// });
-
-// /**
-//  * save data in the database
-//  */
-
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch((error) => {
-//     console.log("Error! ", error);
-//   });
-
-const Task = mongoose.model("Task", {
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid");
-      }
-    },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be a positive value");
-      }
-    },
-  },
-});
-
-const task = new Task({
-  name: "     Alamin Sheikh",
-  email: "alamin@gmail.com",
-});
-
-task
-  .save()
-  .then(() => {
-    console.log(task);
-  })
-  .catch((error) => {
-    console.log("Error!", error);
-  });
