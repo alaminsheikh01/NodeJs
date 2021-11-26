5b58a1935fcff21d7b2f2578e3a53ef1296646ff
alamin <alamin@gmail.com>
2021-11-26 10:30:18 +0600

    working on task

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -25,6 +25,34 @@ app.post("/users", (req, res) => {
 });
 
 /**
+ * find all user
+ */
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((e) => res.status(400).send(e));
+});
+
+/**
+ * find single user data
+ */
+
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch((e) => res.status(500).send(e));
+});
+
+/**
  * save task data from task model
  */
 
@@ -38,6 +66,33 @@ app.post("/task", (req, res) => {
     .catch((e) => {
       res.status(400).send(e);
     });
+});
+
+/**
+ * shows all tasks
+ */
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.send(tasks);
+    })
+    .catch((e) => res.status(500).send(e));
+});
+
+/**
+ * find task by id
+ */
+
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send();
+      }
+      res.send(task);
+    })
+    .catch((e) => res.status(500).send(e));
 });
 
 app.listen(port, () => {
